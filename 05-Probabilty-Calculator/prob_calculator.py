143a13eea9d0e805c5caf5b2b09667812c8b6d1d
Marek Wegierek <mpwegierek@gmail.com>
2023-11-29 14:34:34 +0100

    updated version

--- 05-Probabilty-Calculator/prob_calculator.py
+++ 05-Probabilty-Calculator/prob_calculator.py
@@ -33,12 +33,3 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
 
     return M / num_experiments
 
-
-
-hat = Hat(black=6, red=4, green=3)
-probability = experiment(hat=hat,
-                  expected_balls={"red":2,"green":1},
-                  num_balls_drawn=5,
-                  num_experiments=2000)
-
-print(probability)
